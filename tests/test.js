444ea78bd927112b634f41e6038be72cdb1fe0b7
Tapaswini Sabat <Rajeva.Samantray@lntinfotech.com>
2018-05-24 09:37:25 +0530

    Deleted unwanted files

--- tests/test.js
+++ tests/test.js
@@ -1,20 +1,20 @@
 describe('dataController', function () {
-    //2.
+    
     beforeEach(module('myApp'));
-    //3.
+    
     it('scopeTestSpec',
-        //4.
+        
         inject(function ($controller, $rootScope) {
             var $scope = $rootScope.$new();
             $controller('dataController', {
                 $scope: $scope
             });
-            //5.
+            
             expect($scope.sortType).toEqual('userId');
             expect($scope.sortReverse).toBeDefined();
         expect($scope.sortReverse).toBeFalsy();
         }));
-            //6.
+        
 it('scopeTestSpecFunction',
         inject(function ($controller, $rootScope) {
             var $scope = $rootScope.$new();
